34f2227c8f6c03f21332de867fc22d476b7aefd9
Pavani Kantekari <pk21074@corp.ojas-it.com>
2021-05-14 19:04:00 +0530

    Spring_Security_JWT

---
+++ src/main/java/com/ojas/app/SpringSecurityUsingJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ojas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUsingJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUsingJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ojas/app/SpringSecurityUsingJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojas.app;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityUsingJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
